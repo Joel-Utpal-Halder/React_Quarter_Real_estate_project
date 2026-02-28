809ec9f77f86301ce07236cceefd284904af46e7
Joel Utpal Halder <ju.halder@gmail.com>
2026-02-28 10:33:02 +0600

    feat: update Home.jsx page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,42 @@
+import React from "react";
+import Banner from "../components/home/Banner";
+import FindNow from "../components/home/FindNow";
+import RentalMarketplace from "../components/home/RentalMarketplace";
+import Review from "../components/home/Review";
+import SellsProperties from "../components/home/SellsProperties";
+import Services from "../components/home/Services";
+import PropertiesA from "../components/home/PropertiesA";
+import ApartmentSketch from "../components/home/ApartmentSketch";
+import VideoBanner from "../components/home/VideoBanner";
+import PropertiesB from "../components/home/PropertiesB";
+import Testimonial from "../components/home/Testimonial";
+import NewsBlogs from "../components/home/NewsBlogs";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <FindNow />
+      <RentalMarketplace />
+      <Review />
+      <SellsProperties />
+      <Services />
+      <PropertiesA />
+      <ApartmentSketch />
+      <VideoBanner />
+      <PropertiesB />
+      <Testimonial />
+      <NewsBlogs />
+    </>
+  );
+};
+
+export default Home;
+
+
+
+
+/*
 import React from 'react'
 import Header from '../components/Header'
 import Menu from '../components/Menu'
@@ -41,3 +80,4 @@ const Home = () => {
 }
 
 export default Home
+*/
