cffed4a68fa87eb76d691638e036b9a023cb770f
Joel Utpal Halder <ju.halder@gmail.com>
2026-02-28 15:16:19 +0600

    feat: import and link About page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Home from './pages/Home';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/commonComponents/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <Home />
-    
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
