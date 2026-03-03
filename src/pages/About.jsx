ba975e7b946968920fc770ffc82c5f924c85d56e
Joel Utpal Halder <ju.halder@gmail.com>
2026-03-03 08:06:59 +0600

    feat: add AboutUs.jsx component link to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopPart from '../components/about/TopPart'
+import AboutUs from '../components/about/AboutUs'
 
 const About = () => {
   return (
     <>
       <TopPart />
+      <AboutUs />
     </>
   )
 }
