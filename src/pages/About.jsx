23d43b85be375295f8b228b669a4b075185895e4
Joel Utpal Halder <ju.halder@gmail.com>
2026-03-06 09:28:28 +0600

    feat: import and link Features.jsx component in About.jsx page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopPart from '../components/about/TopPart'
 import AboutUs from '../components/about/AboutUs'
+import { Features } from 'tailwindcss'
 
 const About = () => {
   return (
     <>
       <TopPart />
       <AboutUs />
+      <Features />
     </>
   )
 }
