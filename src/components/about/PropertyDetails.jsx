6012f3d30cf57dabfc6601ec89aeede9a0a01c43
Joel Utpal Halder <ju.halder@gmail.com>
2026-03-16 16:42:09 +0600

    feat (PropertyDetails.jsx): update UI design

--- src/components/about/PropertyDetails.jsx
+++ src/components/about/PropertyDetails.jsx
@@ -4,23 +4,23 @@ const PropertyDetails = () => {
   return (
     <div className="font-nunito relative min-h-screen flex items-center justify-center">
       
-      {/* Background split: white left 1/3, deep ash right 2/3 */}
+      {/* Background */}
       <div className="absolute inset-0 grid grid-cols-3">
         <div className="bg-white"></div>
         <div className="bg-gray-800 col-span-2"></div>
       </div>
       
       {/* Centered card */}
-      <div className="relative z-10 bg-white rounded-sm shadow-gray-400 shadow-xl max-w-3xl w-full p-8 space-y-6">
+      <div className="relative z-9 bg-white rounded-sm shadow-gray-400 shadow-xl max-w-3xl w-200 p-30 space-y-6">
         
         {/* Headline */}
-        <h2 className="text-2xl font-bold text-gray-900 text-feft">
+        <h2 className="text-3xl font-bold text-gray-900 text-left tracking-wide leading-11">
           Your Perfect & Modern <br /> Living Space Waits!
         </h2>
         
-        {/* Icon placed once under headline */}
+        {/* House Icon */}
         <div className="flex justify-center">
-          <img src={houseIcon} alt="house icon" className="w-10 h-10" />
+          <img src={houseIcon} alt="house icon" className="w-10 h-10 -mt-30" />
         </div>
         
         {/* Short description */}
