abebf38d62e992b2bf23ec4b43dd040bbf52076e
Joel Utpal Halder <ju.halder@gmail.com>
2026-03-26 10:02:44 +0600

    feat (PropertyAgents.jsx): update UI design

--- src/components/about/PropertyAgents.jsx
+++ src/components/about/PropertyAgents.jsx
@@ -36,18 +36,20 @@ const PropertyAgents = () => {
           {agents.map((agent, index) => (
             <div
               key={index}
-              className="relative rounded-sm shadow-gray-400 shadow-md transition-transform border-16 border-white"
+              className="relative rounded-sm shadow-gray-400 shadow-md"
             >
-              {/* Full Image fills the card */}
-              <img
-                src={agent.image}
-                alt={agent.name}
-                className="w-full h-96 object-cover z-0"
-              />
+              {/* Card Image Wrapper */}
+              <div className="rounded-sm  border-white border-16 hover:bg-gray-500 hover:shadow-xl">
+                <img
+                  src={agent.image}
+                  alt={agent.name}
+                  className="w-full h-96 object-cover"
+                />
+              </div>
 
               {/* Floating Info Box */}
               <div
-                className="group absolute -bottom-12 left-1/2 transform -translate-x-1/2 w-11/12 max-w-sm px-3 py-1.5 rounded-sm border border-gray-200 shadow-sm flex items-center gap-3 bg-white transition-colors duration-300 hover:bg-orange-500 z-20"
+                className="group absolute -bottom-10 left-1/2 transform -translate-x-1/2 w-10/12 max-w-sm h-28 px-3 py-1.5 rounded-sm border border-gray-200 shadow-sm flex items-center gap-3 bg-white transition-colors duration-300 hover:bg-orange-500 z-20"
               >
                 {/* Social Icons Column */}
                 <div className="flex flex-col gap-1 bg-orange-500 px-1 py-1 rounded-sm transition-colors duration-300 group-hover:bg-white">
@@ -80,11 +82,10 @@ const PropertyAgents = () => {
                 {/* Text on Right */}
                 <div className="text-left transition-colors duration-300 text-gray-800 group-hover:text-white">
                   <h3 className="text-[16px] text-center font-semibold">{agent.name}</h3>
-                  <div className="w-full border-t-[1.5px] border-gray-300 mb-2"></div>
+                  <div className="w-full border-t-[1.5px] border-gray-200 mb-2"></div>
                   <p className="text-xs text-center">{agent.role}</p>
                 </div>
               </div>
-
             </div>
           ))}
         </div>
