deb1642fcaea165423a39dd1c27aba0730c96fa6
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-04 07:03:11 +0600

    feat(App): integrate Services page into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/commonComponents/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
         </Route>
       </Routes>
     </Router>
