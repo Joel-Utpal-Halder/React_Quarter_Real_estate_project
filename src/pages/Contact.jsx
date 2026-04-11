96ede4de5fda7b73f216053709376f2b95f13508
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-11 09:45:54 +0600

    feat(Contact-page): integrate ContactUs component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopPart from '../components/contact/TopPart'
+import ContactUs from '../components/contact/ContactUs'
 
 const Contact = () => {
   return (
     <>
       <TopPart />
+      <ContactUs />
     </>
   )
 }
