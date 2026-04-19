4efcb4f409b1cf20910eafc20a38ba31d854190d
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-19 16:05:42 +0600

    feat(properties): add page with TopPart integration

---
+++ src/pages/Properties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopPart from '../components/properties/TopPart'
+
+const Properties = () => {
+    return (
+        <>
+            <TopPart />
+        </>
+    )
+}
+
+export default Properties
