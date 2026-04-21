73bf256c6d8fe86a3a8efe012186a41b4f5dd388
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-21 12:06:55 +0600

    feat(Properties-page): integrate PropertiesList component

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopPart from '../components/properties/TopPart'
+import PropertiesList from '../components/properties/PropertiesList'
 
 const Properties = () => {
     return (
         <>
             <TopPart />
+            <PropertiesList />
         </>
     )
 }
