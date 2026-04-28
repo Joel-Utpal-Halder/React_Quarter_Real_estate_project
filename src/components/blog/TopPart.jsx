1eeda7eb5ee7704fd9c6bd411e5c293551a81601
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-28 16:20:06 +0600

    feat(TopPart): add component

---
+++ src/components/blog/TopPart.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "../commonComponents/Container";
+import blogPageShape from "../../assets/images/blogPage/blogPageShape.png";
+
+const TopPart = () => {
+    return (
+        <section className="bg-gray-100 font-nunito relative h-[200px] sm:h-[150px] lg:h-[200px]">
+            <Container>
+                <div className="flex items-center justify-start h-full relative">
+                    {/* About us heading */}
+                    <h1
+                        className="text-3xl sm:text-4xl lg:text-5xl font-bold top-14 left-0 absolute transform translate-y-1/2"
+                        style={{ color: "#0A2C3D" }}
+                    >
+                        Blog Details
+                    </h1>
+
+                    {/* Decorative image*/}
+                    <img
+                        src={blogPageShape}
+                        alt="Decorative shape"
+                        className="absolute left-5 -top-40 md:left-140 md:-top-10 lg:left-123 lg:-top-70 md:w-50 md:h-30 lg:w-120 lg:h-100 z-0"
+                    />
+                </div>
+            </Container>
+        </section>
+    );
+};
+
+export default TopPart;
