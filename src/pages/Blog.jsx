d9b8026ffaf136ced7f3153335ed22b7221dee7a
Joel Utpal Halder <ju.halder@gmail.com>
2026-04-28 16:42:36 +0600

    feat(BlogDetails-component): add component with logic and UI design

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopPart from '../components/blog/TopPart'
+import BlogDetails from '../components/blog/BlogDetails'
 
 const Blog = () => {
   return (
     <>
       <TopPart />
+      <BlogDetails />
     </>
   )
 }
